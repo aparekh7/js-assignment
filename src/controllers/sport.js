18cdec0eb48d3a01c1679736175a95d75563b944
aparekh7 <aayush.p@somaiya.edu>
2023-12-13 17:37:41 +0530

    problem 2

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -4,14 +4,20 @@ const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
     matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+        const { sportName, tourName, matchName, matchId, matchStartTime, matchFormat } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        matchDetails = {
+            "matchName": matchName,
+            "matchId": matchId,
+            "matchStartTime": matchStartTime,
+            "matchFormat": matchFormat,
+        }
+        res[sportName][tourName].push(matchDetails);
     });
     return res;
 }
